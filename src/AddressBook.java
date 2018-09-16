98b74f868eac9d10b5b022d41b40c19c95142bd8
Beck <beck-zhang@hotmail.com>
2018-09-16 16:36:19 -0400

    add print out in main

---
+++ src/AddressBook.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+
+public class AddressBook {
+	//field for the class
+	LinkedList<BuddyInfo> list = new LinkedList<BuddyInfo>();
+	
+	
+	public void addBuddy(String name, String addr){
+		BuddyInfo  buddy = new BuddyInfo();
+		buddy.setName(name);
+		buddy.setAddr(addr);
+		list.push(buddy);
+	} 
+	
+	public BuddyInfo removeBuddy(String name){
+		BuddyInfo deletBuddy=null;
+		for(BuddyInfo boddy: list){			
+			if(boddy.getName()==name) {
+				deletBuddy=boddy;
+				list.remove(boddy);
+				break;
+			}
+		}
+		return deletBuddy;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("AddressBook");
+	}
+
+}
