e4792b21a1e4564b171b7f9c0b03149ffd1e85a6
BeckZ <beck-zhang@hotmail.com>
2018-09-23 16:02:16 -0400

    commit by Beck Zhang

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,12 @@
 //This is the hello world java file
+import java.util.*;
+
+public class HelloWorld{
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+}
+
